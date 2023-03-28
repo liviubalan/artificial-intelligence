fb05ef728eed52f903f2a4fb3a7c9779f5f9189d
Liviu Balan <liv_romania@yahoo.com>
2023-03-28 18:22:28 +0300

    Project 2

--- Projects/3_Adversarial Search/my_custom_player.py
+++ Projects/3_Adversarial Search/my_custom_player.py
@@ -1,6 +1,9 @@
 
 from sample_players import DataPlayer
 
+_WIDTH = 11
+_HEIGHT = 9
+_SIZE = (_WIDTH + 2) * _HEIGHT - 2
 
 class CustomPlayer(DataPlayer):
     """ Implement your own agent to play knight's Isolation
@@ -43,4 +46,59 @@ class CustomPlayer(DataPlayer):
         #          call self.queue.put(ACTION) at least once before time expires
         #          (the timer is automatically managed for you)
         import random
-        self.queue.put(random.choice(state.actions()))
+        # self.queue.put(random.choice(state.actions()))
+        SEARCH_DEPTH_MAX = 4
+        EVAL_FUNC = self.centeredness_and_libs
+        if state.ply_count < 2:
+            self.queue.put(random.choice(state.actions()))
+        else:
+            bestMove = self.minimax(state=state, depth=SEARCH_DEPTH_MAX, evalFunc=EVAL_FUNC, playerId=self.player_id)
+            self.queue.put(bestMove)
+
+    def minimax(self, state, depth, evalFunc, playerId):
+        def min_value(state, depth, evalFunc, playerId):
+            if state.terminal_test(): return state.utility(playerId)
+            if depth <= 0: return evalFunc(state, playerId)
+            value = float("inf")
+            for action in state.actions():
+                value = min(value, max_value(state.result(action), depth - 1, evalFunc, playerId))
+            return value
+
+        def max_value(state, depth, evalFunc, playerId):
+            if state.terminal_test(): return state.utility(playerId)
+            if depth <= 0:
+                return evalFunc(state, playerId)
+            value = float("-inf")
+            for action in state.actions():
+                value = max(value, min_value(state.result(action), depth - 1, evalFunc, playerId))
+            return value
+
+        return max(state.actions(), key=lambda x: min_value(state.result(x), depth - 1, evalFunc, playerId))
+
+    def move_difference(self, gameState, playerId):
+        player_loc = gameState.locs[playerId]
+        opp_loc = gameState.locs[1 - playerId]
+        player_libs = gameState.liberties(player_loc)
+        opp_libs = gameState.liberties(opp_loc)
+        return len(player_libs) - len(opp_libs)
+
+    def centeredness(self, gameState, playerId):
+        player_loc = gameState.locs[playerId]
+        opp_loc = gameState.locs[1 - playerId]
+        player_xy = self.computeXy(player_loc)
+        x_centeredness = 5 - abs(5 - player_xy[0])
+        y_centeredness = 4 - abs(4 - player_xy[1])
+        player_centeredness = x_centeredness + y_centeredness
+        opp_xy = self.computeXy(opp_loc)
+        opp_x_cent = 5 - abs(5 - opp_xy[0])
+        opp_y_cent = 4 - abs(4 - opp_xy[1])
+        opp_centeredness = opp_x_cent + opp_y_cent
+        return player_centeredness - opp_centeredness
+
+    def centeredness_and_libs(self, gameState, playerId):
+        center_score = self.centeredness(gameState, playerId)
+        liberties_score = self.move_difference(gameState, playerId)
+        return liberties_score * 2 + center_score * 1
+
+    def computeXy(self, ind):
+        return (ind % (_WIDTH + 2), ind // (_WIDTH + 2))
